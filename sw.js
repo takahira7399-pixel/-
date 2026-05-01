9184e74397050c13cfaf248cdb5128a44d740d70
takahira7399-pixel <takahira7399@gmail.com>
2026-05-01 14:40:48 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,35 @@
+var CACHE_NAME = 'kaiten-v2';
+var FILES = [
+  './pachinko.html',
+  './manifest.json',
+  './icon.png'
+];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(FILES);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(
+        keys.filter(function(k) { return k !== CACHE_NAME; })
+            .map(function(k) { return caches.delete(k); })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(cached) {
+      return cached || fetch(e.request);
+    })
+  );
+});
